ad2adfd46b429e764e11cf592561652717019c6d
ROBERTO CORDOVA <ROBCORDO@uat.edu>
2019-12-11 11:51:12 -0700

    Add project files.

---
+++ HeaderSourceCode/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+void main()
+{
+	int a = 5;
+	int b = 6;
+
+	int c = add(a, b);
+
+	cout << c << endl;
+}
+
---
+++ HeaderSourceCode/header.cpp
@@ -0,0 +1,6 @@
+#include "Header.h"
+
+int add(int a, int b)
+{
+	return a + b;
+}
